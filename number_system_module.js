81e935b2c0edc10a0fbb1f94b1fd65712c40bb77
Richard <70591839+ric46@users.noreply.github.com>
2023-09-16 19:43:06 +0530

    js changes

--- number_system_module.js
+++ number_system_module.js
@@ -1,39 +1,37 @@
 "use strict"
-// start implementing all the functions
+
 
 const dec_to_bin = (N) => {
-    // N is given as a decimal number, convert it to the corresponding binary number.
+    
     let ans = N.toString(2);
-    // You should return the correct binary number.
     return ans
 }
 const bin_to_dec = (N) => {
-    // N is given as a binary number, convert it to the corresponding decimal number.
+   
     let ans=parseInt(N,2)
-    // You should return the correct binary decimal.
     return ans
 }
 
 const dec_to_oct = (N) => {
-    // N is given as a decimal number, convert it to the corresponding octal number.
+    
     let oct = 0;
     let i = 0;
     while (N != 0) {
-        oct += (N % 8) * Math.pow(10, i); // storing and reversing the remainder sequence
+        oct += (N % 8) * Math.pow(10, i);
         N = parseInt(N / 8);
         i++;
     }  
-    // You should return the correct octal number.
+    
     return oct;
 }
 const oct_to_dec = (N) => {
-    // N is given as a octal number, convert it to the corresponding decimal number.
+    
      let ans=parseInt(N,8)
-    // You should return the correct decimal number.
+   
     return ans;
 }
 const dec_to_hex = (N) => {
-    // N is given as a decimal number, convert it to the corresponding hexadecimal number.
+    
     let hex = "";
     while (N != 0) {
         let rem = N % 16
@@ -62,56 +60,56 @@ const dec_to_hex = (N) => {
         }
         N = parseInt(N / 16);
     }
-    // You should return the correct hexadecimal number.
+    
     return hex.split("").reverse().join(""); 
 }
 
 const hex_to_dec = (N) => {
-    // N is given as a hexadecimal number, convert it to the corresponding decimal number.
+    
     let ans=parseInt(N,16)
-    // You should return the correct binary number.
+    
     return ans
 }
 const bin_to_oct = (N) => {
-    // N is given as a binary number, convert it to the corresponding octal number.
-    // Implement this function using two step method
+    
+    
     N = bin_to_dec(N);
-    // You should return the correct octal number.
+    
     return dec_to_oct(N);
 }
 const bin_to_hex = (N) => {
-    // N is given as a binary number, convert it to the corresponding hexadecimal number.
-    // Implement this function using two step method
+    
+    
     N = bin_to_dec(N)
-    // You should return the correct hexadecimal number.
+    
     return dec_to_hex(N)
 }
 const oct_to_bin = (N) => {
-    // N is given as a octal number, convert it to the corresponding binary number.
-    // Implement this function using two step method
+    
+    
     N = oct_to_dec(N)
-    // You should return the correct binary number.
+    
     return dec_to_bin(N)
 }
 const oct_to_hex = (N) => {
-    // N is given as a octal number, convert it to the corresponding hexadecimal number.
-    // Implement this function using two step method
+    
+    
     N = oct_to_dec(N)
-    // You should return the correct hexadecimal number.
+    
     return dec_to_hex(N)
 }
 const hex_to_bin = (N) => {
-    // N is given as a hexadecimal number, convert it to the corresponding binary number.
-    // Implement this function using two step method
-   N = hex_to_dec(N)
-    // You should return the correct binary number.
+    
+    
+    N = hex_to_dec(N)
+    
     return dec_to_bin(N)
 }
 const hex_to_oct = (N) => {
-    // N is given as a hexadecimal number, convert it to the corresponding octal number.
-    // Implement this function using two step method
+    
+    
     N = hex_to_dec(N)
-    // You should return the correct octal number.
+    
     return dec_to_oct(N)
 }
 
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict"
-// Start writing code from here
-// Declare all the global variables
+
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 const input_option = document.querySelector("#input-options")
@@ -8,14 +7,14 @@ const output_option = document.querySelector("#output-options")
 const reset_btn	 = document.querySelector("#reset")
 const calc_btn = document.querySelector("#calc")
 const swap_btn = document.querySelector("#swap")
-// You should define the callback function for the RESET button here.
+
 reset_btn.addEventListener("click", function () {
     input.value=""
     output.value=""
     input_option.selectedIndex=0
     output_option.selectedIndex=0
 });
-// You should define the callback function for the SWAP button here.
+
 swap_btn.addEventListener("click", function () {
     let temp = input_option.selectedIndex;
     input_option.selectedIndex = output_option.selectedIndex;
@@ -25,7 +24,7 @@ swap_btn.addEventListener("click", function () {
     output.value = temp
 });
 
-// You should define the callback function for the CALCULATE button here.
+
 calc_btn.addEventListener("click", function () {
     let input_no_system = input_option[input_option.selectedIndex].value;
     let output_no_system = output_option[output_option.selectedIndex].value;
@@ -34,9 +33,9 @@ calc_btn.addEventListener("click", function () {
         ans = input.value;
     }
     else if (input_no_system == "Decimal") {
-        input_val = parseInt(input.value); // Converting String to integer because our expects an integer
+        input_val = parseInt(input.value); 
         if (output_no_system == "Binary") {
-            ans = dec_to_bin(input_val); // Calling proper function and storing correct output in ans variable.
+            ans = dec_to_bin(input_val); 
         }
         else if (output_no_system == "Octal") {
             ans = dec_to_oct(input_val);
@@ -46,9 +45,9 @@ calc_btn.addEventListener("click", function () {
         }
     }
     else if (input_no_system == "Binary") {
-        input_val = parseInt(input.value); // Converting String to integer because our expects an integer
+        input_val = parseInt(input.value); 
         if (output_no_system == "Decimal") {
-            ans = bin_to_dec(input_val); // Calling proper function and storing correct output in ans variable.
+            ans = bin_to_dec(input_val); 
         }
         else if (output_no_system == "Octal") {
             ans = bin_to_oct(input_val);
@@ -58,9 +57,9 @@ calc_btn.addEventListener("click", function () {
         }
     }
     else if (input_no_system == "Octal") {
-        input_val = parseInt(input.value); // Converting String to integer because our expects an integer
+        input_val = parseInt(input.value);
         if (output_no_system == "Decimal") {
-            ans = oct_to_dec(input_val); // Calling proper function and storing correct output in ans variable.
+            ans = oct_to_dec(input_val); 
         }
         else if (output_no_system == "Binary") {
             ans = octbin_to_bin(input_val);
@@ -70,7 +69,7 @@ calc_btn.addEventListener("click", function () {
         }
     }
     else if(input_no_system == "Hexadecimal"){
-        input_val = input.value; // No need of String-Integer typecasting
+        input_val = input.value; 
         if (output_no_system == "Decimal") {
             ans = hex_to_dec(input_val);
         }
